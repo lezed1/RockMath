dc85cc146843f603b211d95e480931d54b16f121
lezed1 <lezed1@gmail.com>
2013-06-11 11:09:11 -0400

    Inital Commit
    Started from Template

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+
+#define MY_UUID { 0x89, 0x91, 0x5B, 0x7E, 0xA0, 0xFB, 0x44, 0xC0, 0xBB, 0xE3, 0xE5, 0x0B, 0xD1, 0xC6, 0x47, 0xBA }
+PBL_APP_INFO(MY_UUID,
+             "Template App", "Your Company",
+             1, 0, /* App version */
+             DEFAULT_MENU_ICON,
+             APP_INFO_STANDARD_APP);
+
+Window window;
+
+
+void handle_init(AppContextRef ctx) {
+  (void)ctx;
+
+  window_init(&window, "Window Name");
+  window_stack_push(&window, true /* Animated */);
+}
+
+
+void pbl_main(void *params) {
+  PebbleAppHandlers handlers = {
+    .init_handler = &handle_init
+  };
+  app_event_loop(params, &handlers);
+}
+
